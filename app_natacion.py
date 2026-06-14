bda90a819e9cf75c5b1bb13765fa9064b70d6eae
alvarogallegosz <alvarogallegosz@gmail.com>
2026-06-14 18:26:30 -0400

    Update app_natacion.py

--- app_natacion.py
+++ app_natacion.py
@@ -31,13 +31,13 @@ titulo_grafico = st.sidebar.text_input("Estilo y Distancia:", value="100 Libre")
 
 st.sidebar.subheader("⏳ Hitos de Edad")
 t0 = st.sidebar.number_input("Edad Start (t0):", min_value=5.0, max_value=20.0, value=9.0, step=0.01)
-t_pb = st.sidebar.number_input("Edad del PB Actual (t_pb):", min_value=t0, max_value=30.0, value=9.0, step=0.01)
+t_pb = st.sidebar.number_input("Edad del PB Actual (t_pb):", min_value=t0, max_value=30.0, value=11.0, step=0.01)
 t_peak = st.sidebar.number_input("Edad Peak Proyectado (t_peak):", min_value=t_pb, max_value=30.0, value=23.0, step=0.01)
 
 st.sidebar.subheader("⏱️ Hitos de Tiempo")
-T0 = st.sidebar.number_input("Tiempo Inicial (T0 en segundos):", min_value=1.0, value=1.0, step=0.1)
-T_pb = st.sidebar.number_input("Tiempo del PB Actual (T_pb en segundos):", min_value=1.0, value=1.0, step=0.01)
-T_target = st.sidebar.number_input("Tiempo Objetivo Peak (T_target en segundos):", min_value=1.0, value=1.0, step=0.01)
+T0 = st.sidebar.number_input("Tiempo Inicial (T0 en segundos):", min_value=1.0, value=90.0, step=0.1)
+T_pb = st.sidebar.number_input("Tiempo del PB Actual (T_pb en segundos):", min_value=1.0, value=70.0, step=0.01)
+T_target = st.sidebar.number_input("Tiempo Objetivo Peak (T_target en segundos):", min_value=1.0, value=54.0, step=0.01)
 
 st.sidebar.subheader("🎛️ Amortiguación de Deriva")
 h = st.sidebar.slider("Factor de deriva manual (h):", min_value=0.1, max_value=1.0, value=0.3, step=0.05)
