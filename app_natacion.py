361c4ba660714413f58de4588cba32e31860e6ef
alvarogallegosz <alvarogallegosz@gmail.com>
2026-06-24 12:47:22 -0400

    Update app_natacion.py

--- app_natacion.py
+++ app_natacion.py
@@ -1189,10 +1189,12 @@ if len(df_procesado) > 0 or modo_equipo:
     export_df = df_procesado.drop(columns=["id", "usuario_id"], errors="ignore")
     csv_data = export_df.to_csv(index=False).encode('utf-8')
     txt_string = export_df.to_string(index=False)
-    
-    img_buffer = io.BytesIO()
-    fig.savefig(img_buffer, format="png", bbox_inches=None, dpi=300)
-    img_buffer.seek(0)
+    if not atletas_filtrados:
+        st.warning("No se encontraron atletas activos con los criterios de segmentación elegidos.")
+    else
+        img_buffer = io.BytesIO()
+        fig.savefig(img_buffer, format="png", bbox_inches=None, dpi=300)
+        img_buffer.seek(0)
     
     c_exp1, c_exp2, c_exp3 = st.columns(3)
     with c_exp1:
